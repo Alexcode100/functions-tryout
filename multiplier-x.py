c95cd0b3aeba9ce6c85b32cd5fad51d2755e752c
Alexcode100 <89846069+Alexcode100@users.noreply.github.com>
2021-11-01 10:01:03 +0000

    Yes

---
+++ multiplier-x.py
@@ -0,0 +1,5 @@
+def BerekeningTafel(GetalVraag = int(input('Van welk getal wilt u de tafel zien?'))):
+    while GetalVraag < 11:
+        print(GetalVraag)
+        GetalVraag = GetalVraag + 1
+BerekeningTafel()
